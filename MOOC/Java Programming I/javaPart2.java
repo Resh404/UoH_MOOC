6a6fb6600e3d9b3503175b25d73548d142a42038
RZ <38974894+Resh404@users.noreply.github.com>
2023-10-29 18:57:17 +0100

    Add files via upload

---
+++ MOOC/Java Programming I/javaPart2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+//Exercises from Java Programming I part 2 (https://java-programming.mooc.fi/)
+public class javaPart2 {
+    public static void main(String[] args){
+        
+    }
+}
