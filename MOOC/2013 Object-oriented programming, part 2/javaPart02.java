5fac15c6e621709d917d30567e81145515fbad46
RZ <Reshad_z@hotmail.com>
2023-10-30 16:01:49 +0100

    update

---
+++ MOOC/2013 Object-oriented programming, part 2/javaPart02.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class javaPart02 {
+    public static void main(String[] args){
+
+    }
+}
--- MOOC/2013 Object-oriented programming, part 2/javaPart2.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class javaPart02 {
-    public static void main(String[] args){
-
-    }
-}
